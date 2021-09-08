691f72912ca059b9475b48e924a49bd61d24e66a
dhanush <dhanuu08@gmail.com>
2021-09-08 13:43:38 +0530

    added new method for bubblesort

--- Arrays/Sorting/bubble_sort.py
+++ Arrays/Sorting/bubble_sort.py
@@ -23,5 +23,16 @@ def bubble_sort_recursive(arr):
     return bubble_sort_recursive(arr[0:-1])+[arr[-1]]
 
 
-arr = [64, 34, 25, 12, 22, 11, 90]
-print(bubble_sort_recursive(arr))
+def bubble_sort_recursive1(arr, index):
+    if index < 0:
+        return arr
+
+    for i in range(0, index):
+        if(arr[i] > arr[i+1]):
+            arr[i], arr[i+1] = arr[i+1], arr[i]
+
+    return bubble_sort_recursive1(arr, index-1)
+
+
+arr = [64, 34, 25, 12, 22, 90, 11]
+print(bubble_sort_recursive1(arr, len(arr)-1))
